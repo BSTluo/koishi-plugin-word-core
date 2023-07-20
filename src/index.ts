3010c150ff38229dcc56cd813c4df14e8aed4ce2
BSTluo <1946831552@qq.com>
2023-07-20 11:18:36 +0800

    new file:   src/api/index.ts 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,72 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'word-core'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+// TypeScript 用户需要进行类型合并
+declare module 'koishi' {
+  interface Tables {
+    wordCoreConfig: wordCoreConfig
+    wordUserData: wordUserData
+    wordData: wordData
+    recycleBinList: recycleBinList
+  }
+}
+
+export interface wordCoreConfig {
+  id: string
+  timeTemp: JSON
+  permission: string[]
+}
+
+export interface wordUserData {
+  id: string
+  item: JSON
+}
+
+export interface wordData {
+  id: string
+  data: JSON
+}
+
+export interface recycleBinList {
+  id: string
+  data: JSON
+}
+
+const dbInit = (ctx: Context) => {
+  ctx.model.extend('wordCoreConfig', {
+    id: 'string',
+    timeTemp: 'json',
+    permission: 'list'
+  }, {
+    primary: 'id'
+  })
+
+  ctx.model.extend('wordUserData', {
+    id: 'string',
+    item: 'json'
+  }, {
+    primary: 'id'
+  })
+
+  ctx.model.extend('wordData', {
+    id: 'string',
+    data: 'json'
+  }, {
+    primary: 'id'
+  })
+
+  ctx.model.extend('recycleBinList', {
+    id: 'string',
+    data: 'json'
+  }, {
+    primary: 'id'
+  })
+}
+
+export async function apply(ctx: Context) {
+  dbInit(ctx)
 }
