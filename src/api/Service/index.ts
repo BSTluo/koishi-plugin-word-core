75efce1df062407f2fdfd2b3b29a9fd20976e5e0
BSTluo <1946831552@qq.com>
2023-10-18 15:21:27 +0800

    modified:   src/api/Service/Editor/Editor.ts 	modified:   src/api/Service/index.ts 	modified:   src/api/index.ts

--- src/api/Service/index.ts
+++ src/api/Service/index.ts
@@ -6,41 +6,41 @@ import * as Cache from "./Editor/Cache";
 export * from './Tools/Tools';
 
 declare module 'koishi' {
-    interface Context {
-        word: word;
-    }
+  interface Context {
+    word: word;
+  }
 }
 
 export class word extends Service {
-    Tools: Tools.ToolsFunction = {
-        async readDB(dbName, key) { },
-        async writeDB(dbName, key, data) { return true; },
-        getDB(dbName) { },
-    };
+  Tools: Tools.ToolsFunction = {
+    async readDB(dbName, key) { },
+    async writeDB(dbName, key, data) { return true; },
+    getDB(dbName) { },
+  };
 
-    User: User.UserFunction = {
-        async getData(uid) { return { a: { a: 1 } }; },
-        async updateData(uid, data) { return true; },
-        async getItem(uid, cell, item) { return null; },
-        async updateItem(uid, cell, itemName, amount) { return true; }
-    };
+  User: User.UserFunction = {
+    async getData(uid) { return { a: { a: 1 } }; },
+    async updateData(uid, data) { return true; },
+    async getItem(uid, cell, item) { return null; },
+    async updateItem(uid, cell, itemName, amount) { return true; }
+  };
 
-    Cache: Cache.CacheFunction = {
-        async getCache() { return { hasKey: {} }; }
-    };
+  Cache: Cache.CacheFunction = {
+    async getCache() { return { hasKey: {} }; }
+  };
 
-    constructor(ctx: Context) {
-        // 这样写你就不需要手动给 ctx 赋值了
-        super(ctx, 'word', true);
-        this.ctx = ctx;
+  constructor(ctx: Context) {
+    // 这样写你就不需要手动给 ctx 赋值了
+    super(ctx, 'word', true);
+    this.ctx = ctx;
 
-        this.Tools.readDB = (dbName, key) => { return Tools.readDBFunction(this.ctx, dbName, key); };
-        this.Tools.writeDB = (dbName, key, data) => { return Tools.writeDBFunction(this.ctx, dbName, key, data); };
-        this.Tools.getDB = (dbName) => { return Tools.getDBFunction(ctx, dbName); };
+    this.Tools.readDB = (dbName, key) => { return Tools.readDBFunction(this.ctx, dbName, key); };
+    this.Tools.writeDB = (dbName, key, data) => { return Tools.writeDBFunction(this.ctx, dbName, key, data); };
+    this.Tools.getDB = (dbName) => { return Tools.getDBFunction(ctx, dbName); };
 
-        this.User.getData = (uid) => { return User.getData(this.Tools.readDB, uid); };
-        this.User.updateData = (uid, data) => { return User.updateData(this.Tools.writeDB, uid, data); };
-        this.User.getItem = (uid, cell, item) => { return User.getItem(this.Tools.readDB, uid, cell, item); };
-        this.User.updateItem = (uid, cell, itemName, amount) => { return User.updateItem(this.Tools.readDB, this.Tools.writeDB, uid, cell, itemName, amount); };
-    }
+    this.User.getData = (uid) => { return User.getData(this.Tools.readDB, uid); };
+    this.User.updateData = (uid, data) => { return User.updateData(this.Tools.writeDB, uid, data); };
+    this.User.getItem = (uid, cell, item) => { return User.getItem(this.Tools.readDB, uid, cell, item); };
+    this.User.updateItem = (uid, cell, itemName, amount) => { return User.updateItem(this.Tools.readDB, this.Tools.writeDB, uid, cell, itemName, amount); };
+  }
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -4,72 +4,72 @@ import { word } from './Service/index';
 export const using = ['database'];
 
 declare module 'koishi' {
-    interface Tables {
-        wordUserData: wordUserData;
-        wordData: wordData;
-        recycleBinList: recycleBinList;
-        wordUserConfig: wordUserConfig;
-    }
+  interface Tables {
+    wordUserData: wordUserData;
+    wordData: wordData;
+    recycleBinList: recycleBinList;
+    wordUserConfig: wordUserConfig;
+  }
 }
 
 export interface wordSaveData {
-    saveDB: string;
-    author: string[];
-    data: Record<string, string[]>;
+  saveDB: string;
+  author: string[];
+  data: Record<string, string[]>;
 }
 
 export interface wordUserConfig {
-    id: string;
-    data: Record<string, string>;
+  id: string;
+  data: Record<string, string>;
 }
 
 export interface wordUserData {
-    id: string;
-    data: Record<string, string>;
+  id: string;
+  data: Record<string, string>;
 }
 
 export interface wordData {
-    id: string;
-    data: wordSaveData;
+  id: string;
+  data: wordSaveData;
 }
 
 export interface recycleBinList {
-    id: string;
-    data: wordSaveData;
+  id: string;
+  data: wordSaveData;
 }
 
 const dbInit = (ctx: Context) => {
 
-    ctx.model.extend('wordUserData', {
-        id: 'string',
-        data: 'json'
-    }, {
-        primary: 'id'
-    });
+  ctx.model.extend('wordUserData', {
+    id: 'string',
+    data: 'json'
+  }, {
+    primary: 'id'
+  });
 
-    ctx.model.extend('wordData', {
-        id: 'string',
-        data: 'json'
-    }, {
-        primary: 'id'
-    });
+  ctx.model.extend('wordData', {
+    id: 'string',
+    data: 'json'
+  }, {
+    primary: 'id'
+  });
 
-    ctx.model.extend('wordUserConfig', {
-        id: 'string',
-        data: 'json'
-    }, {
-        primary: 'id'
-    });
+  ctx.model.extend('wordUserConfig', {
+    id: 'string',
+    data: 'json'
+  }, {
+    primary: 'id'
+  });
 
-    ctx.model.extend('recycleBinList', {
-        id: 'string',
-        data: 'json'
-    }, {
-        primary: 'id'
-    });
+  ctx.model.extend('recycleBinList', {
+    id: 'string',
+    data: 'json'
+  }, {
+    primary: 'id'
+  });
 };
 
 export const apply = (ctx: Context) => {
-    dbInit(ctx);
-    ctx.plugin(word);
+  dbInit(ctx);
+  ctx.plugin(word);
 };
