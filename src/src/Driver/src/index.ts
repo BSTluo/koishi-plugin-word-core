d436a6f57d24f361f0350e9db7f1f6246330afe4
BSTluo <1946831552@qq.com>
2023-11-24 15:56:49 +0800

    up

--- src/src/Driver/src/index.ts
+++ src/src/Driver/src/index.ts
@@ -7,11 +7,34 @@ export const parsStart = (questionList: string[]) => {
 
   let temp = getRandQuestion(questionList);
 
-  const msg = par(temp);
-};
-
-const par = (str: string) => {
   // 先将文本拆解为树
   // 你(+:xx:xx)好
   // [你,[+,xx,xx],好]
+  const msg = parse(temp);
+
+  // 再进行树的解析
 };
+
+const parse = (str: string): any[] => {
+  const tempArr: any[] = [];
+  let index = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const v = str[i];
+
+    if (v === '(') {
+      index++;
+      const strTemp = str.substring(i);
+      tempArr[index] = parse(strTemp);
+    } else if (v === ')') {
+      index++;
+      return tempArr;
+    } else {
+      if (!tempArr[index]) { tempArr[index] = ''; }
+      tempArr[index] += v;
+    }
+  }
+
+  return tempArr;
+};
+
