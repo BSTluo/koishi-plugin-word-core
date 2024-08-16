94d31b4798bd6c77f02e6fa15cf6a265b0b47827
BSTluo <1946831552@qq.com>
2024-08-16 14:27:53 +0800

    更新了使用说明

--- src/index.ts
+++ src/index.ts
@@ -32,8 +32,7 @@ export const Config: Schema<Config> = Schema.object({
 export const logger = new Logger('Word-core');
 
 export const usage = `词库的教程文档在这里：https://docs.reifuu.icu/docs/word-core-3.0/00readme/
-此版本修复了一个bug，暂未进行测试，如果发现对以往的命令无法正常解析的时候！请在论坛或者发送邮件给我，并且恢复v26版本！
-另外词库暂时会有污染上下文的问题出现，这不是预期的！正在等待官方修复！应该会很快！
+如果有问题的话欢迎联系我！
 邮箱：1946831552@qq.com`;
 
 // TypeScript 用户需要进行类型合并
